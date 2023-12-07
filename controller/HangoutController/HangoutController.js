9aee00f6c8b3b891004947c6009a238e46dd1721
geraldakbar <mgeraldakbar@gmail.com>
2023-12-07 19:10:49 +0700

    [ADD] Hangout Endpoints

--- controller/HangoutController/HangoutController.js
+++ controller/HangoutController/HangoutController.js
@@ -6,3 +6,40 @@ const HangoutService = require("../../services/HangoutService/HangoutService")
  * @typedef {import("express").NextFunction} NextFunction
  */
 
+/**
+ * @param {Request} req
+ * @param {Response} res
+ */
+async function getHangoutById(req,res){
+
+    try {
+        const hangoutId = req.params.hangoutId;
+        const userId = req.userId;
+        res.status(200).send(await HangoutService.getHangoutDetails({
+            userId: userId,
+            hangoutId: hangoutId,
+        }));
+    } catch(error) {
+        res.status(400).send({
+            "message": error.message,
+        })
+    }
+}
+
+async function getAvailableHangouts(req,res) {
+    try {
+        const userId = req.userId;
+        const hangouts = await HangoutService.getHangoutAvailable(userId);
+
+        res.status(200).send(hangouts);
+    } catch(error) {
+        res.status(400).send({
+            "message": error.message,
+        })
+    }
+}
+
+module.exports =  {
+    getAvailableHangouts,
+    getHangoutById
+}
--- services/HangoutService/HangoutService.js
+++ services/HangoutService/HangoutService.js
@@ -1,22 +1,167 @@
-async function publishHangout(){
+const { UserModel } = require("../../model");
+const HangoutModel = require("../../model/HangoutModel/HangoutModel");
+const UserEventsModel = require("../../model/UserModel/UserEventsModel");
+const UserService = require("../UserService/UserService")
 
+/**
+ * @typedef {Object} AddHangoutArgs
+ * @property {string} userId
+ * @property {string} title
+ * @property {string} description
+ * @property {string} imageURL
+ * @property {number} estimatedOutput 
+ */
+/**
+ * @typedef {Object} JoinHangoutArgs
+ * @property {string} userId
+ * @property {string} hangoutId
+ */
+
+/**
+ * @typedef {Object} updateHangoutArgs
+ * @property {string} eventId
+ */
+
+
+/**@param {AddHangoutArgs} args */
+async function publishHangout(args){
+    const eventId = generateUniqueId();
+
+    const hangout = await HangoutModel.create({
+        eventId: eventId,
+        title: args.title,
+        description: args.description,
+        imageURL: args.imageURL,
+        estimatedOutput: args.estimatedOutput,
+    })
+
+    await UserEventsModel.create({
+        userId: args.userId,
+        eventId: eventId,
+    })
+
+    return hangout;
 }
 
-async function joinHangout(){
+/**@param {JoinHangoutArgs} args */
+async function joinHangout(args){
+    const userId = args.userId;
+    const eventId = args.hangoutId;
 
+    const user = await UserModel.findOne({
+        where: {
+            userId: userId,
+        }
+    })
+
+    if (!user) {
+        throw new Error("User Not Found");
+    }
+
+    const result = await UserEventsModel.create({
+        userId: userId,
+        eventId: eventId,
+    })
+
+    return result;
 }
 
-async function updateHangoutStatus(){
+async function getUserHangout(userId){
+    const hangouts = await UserModel.findOne({
+        where: {
+            userId: userId,
+        },
+        include: {
+            model: HangoutModel,
+            through: UserEventsModel,
+            as: "Hangouts",
+        }
+    })
 
+    return hangouts;
 }
 
-async function getHangoutAvailable(){
+async function getHangoutAvailable(userId){
+    const user = await UserModel.findOne({
+        where: {
+            userId: userId,
+        }
+    })
 
+    if (!user) {
+        throw new Error("User Not Found");
+    }
+
+    const friends = await UserService.getAllFriends(userId);
+
+    let hangouts = [];
+
+    await Promise.all(friends.forEach(async (friend) => {
+        const hangoutList = await getUserHangout(friend.userId);
+
+        hangoutList.forEach((hangout) => {
+            hangouts.push(hangout.getDataValue("Hangouts"));
+        })
+    }));
+
+    return hangouts;
 }
+
+/**@param {JoinHangoutArgs} args*/
+async function getHangoutDetails(args){
+    const user = await UserModel.findOne({
+        where: {
+            userId: args.userId,
+        }
+    })
+
+    if (!user) {
+        throw new Error("User Not Found");
+    }
+
+    const event = await HangoutModel.findOne({
+        where: {
+            eventId: args.hangoutId,
+        }
+    })
+
+    if (!event) {
+        throw new Error("Hangout Not Found");
+    }
+
+    const userHasJoined = await UserEventsModel.findOne({
+        where: {
+            userId: args.userId,
+            eventId: args.hangoutId,
+        }
+    })
+
+    if (!userHasJoined) {
+        event['joined'] = false;
+    } else {
+        event['joined'] = true;
+    }
+
+    return event;
+}
+
+function generateUniqueId() {
+    // Get the current timestamp
+    const timestamp = new Date().getTime();
+  
+    // Generate a random number (you can use a more sophisticated method if needed)
+    const random = Math.floor(Math.random() * 10000);
+  
+    // Concatenate timestamp and random number to create a unique ID
+    const uniqueId = `${timestamp}${random}`;
+  
+    return "ev_" + uniqueId;
+  }
 
 module.exports = {
     publishHangout,
     joinHangout,
-    updateHangoutStatus,
     getHangoutAvailable,
+    getHangoutDetails,
+    getUserHangout,
 }
